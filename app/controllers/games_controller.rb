b27fedf22220b5094ee340d74d2f848856efb4fd
David Cisneros <d.cisneros849@gmail.com>
2016-11-05 22:37:31 -0700

    added log in and sign up links

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,10 +4,27 @@ class GamesController < ApplicationController
   end
 
   def new
-    @game
+    @game = Game.new
   end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+  if @game.save
+      redirect_to user_path(current_user)
+  else
+    render :new
+  end
+end
+
 
   def show
     @game = Game.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:date, :time, :seats_available, :price, :comments, :start_location, :end_location, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'games/index'
-
-  get 'games/new'
-
-  get 'games/show'
-
   root 'home#index'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, :only => [:show]
+  resources :games 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
