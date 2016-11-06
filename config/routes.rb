c5b0fe0d856c6d0019f9acc44a17a00a175ace44
David Cisneros <d.cisneros849@gmail.com>
2016-11-05 23:12:49 -0700

    got devise log out to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
+
   resources :users, :only => [:show]
-  resources :games 
+  resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
