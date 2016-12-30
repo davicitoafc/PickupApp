56a7384c14a2a6583c7c54d09cc1504018866bea
David Cisneros <d.cisneros849@gmail.com>
2016-12-29 19:38:29 -0800

    commented code on map.js and changed nav color

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,20 +1,24 @@
+//Initiate Map
 function initMap() {
+  //Create map variable that grabs div by the id "map"
   var map = new google.maps.Map(document.getElementById('map'), {
+    //Sets map zoom to 9
     zoom: 9,
+    //Default lat lng of San Francisco
     center: {lat: 37.7749, lng: -122.4194}
   });
 
   var infoWindow = new google.maps.InfoWindow({map: map});
 
 
-         // Try HTML5 geolocation.
+         // Condition that grabs users geolocation using HTML5
          if (navigator.geolocation) {
            navigator.geolocation.getCurrentPosition(function(position) {
              var pos = {
                lat: position.coords.latitude,
                lng: position.coords.longitude
              };
-
+        //If it passes, the map centers at pos
              map.setCenter(pos);
 
            }, function() {
@@ -25,25 +29,25 @@ function initMap() {
            handleLocationError(false, infoWindow, map.getCenter());
          }
 
-
+         //AJAX call that GET requests JSON data of games
         $.ajax({
                type: "GET",
                dataType: "json",
                url: "/games",
                success: function(data){
+
+              //Code to get todays date
                  var today = new Date();
                  var dd = today.getDate();
-                 var mm = today.getMonth()+1; //January is 0!
+                 var mm = today.getMonth()+1;
                  var yyyy = today.getFullYear();
 
                  if(dd<10) {
                      dd='0'+dd
                  }
-
                  if(mm<10) {
                      mm='0'+mm
                  }
-
                  today = yyyy+'-'+mm+'-'+dd
                 // loop through data to retrieve games
                for( var i=0; i<data.length; i++ ){
@@ -62,24 +66,26 @@ function initMap() {
                    date: data[i].date
                   });
 
+                  // Conditional statement to set marker to null if todays date is greater than game date
                   if (data[i].date < today) {
                     marker.setMap(null);
                   }
-
+                //Event listener to allow user to zoom in on marker with 1 click
                 google.maps.event.addListener(marker, 'click', function() {
                       map.setCenter(this.position);
                       map.setZoom(15);
                 });
-
+                // Variable with boolean for infowindow
                 var previousWindow = false
-
+                //Even listener for mouseoever of marker, which pops up game info in infowindow
                 google.maps.event.addListener(marker, 'mouseover', function() {
 
+                //Variables to grab values of marker properties
                    var gameLocation = this.title
                    var gameType = this.category
                    var gamePlayers = this.players/2
                    var gameDate = this.date
-
+                //Variable with infowindow information that will load game details
                    var gameInfo = '<div class="content" style="font-size: 10px;text-align:center;">' +
                         '<h3 style="font-size: 20px;">Game Location: <p style="font-size: 13px;">' + gameLocation + '</p></h3>' +
                         '<h3 style="font-size: 20px;">Players: <br>' + gamePlayers + ' vs ' + gamePlayers + ' </h3>' +
@@ -87,17 +93,18 @@ function initMap() {
                         '<h3 style="font-size: 20px;">Date: ' + gameDate + ' </h3>' +
                         '<a style="font-size: 20px;" href="/games/'+ this.id + '">View Game</a>' +
                       '</div>';
-
+                  //InfoWindow that holds gameInfo as a property
                       var infowindow = new google.maps.InfoWindow({
                          content: gameInfo
                       });
-
+                  //Conditional statement to check if previousWindow is true
                       if (previousWindow) {
+                  //If true, .close will close the window, so multiple windows dont open after consecutive mouseovers
                         previousWindow.close();
                       }
 
                       previousWindow = infowindow
-
+                    //Inforwindow that opens on map via the marker location
                    infowindow.open(map, this);
                 }); // marker listener
               } // loop for objects
