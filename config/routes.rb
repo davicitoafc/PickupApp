b64ae4558aa829e568d179e9168b025eb513b5a6
David Cisneros <d.cisneros849@gmail.com>
2016-12-31 22:28:12 -0800

    added fb oath

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }, { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { registrations: 'registrations',
+    :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :users, :only => [:show]
   resources :games
