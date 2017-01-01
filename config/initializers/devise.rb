c626fa2bcd299903c19720710a3c95b1bf677393
David Cisneros <d.cisneros849@gmail.com>
2016-12-31 21:38:09 -0800

    added ENV variables, configured routes and created controller for oamniouth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,8 +248,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "APP_ID", "APP_SECRET",
-                  callback_url: "CALLBACK_URL"
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'],
+                  callback_url: ENV['callback_url']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
