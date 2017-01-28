ecbf4a19cab36ebca2474d46bdcbc55ddcbbe6df
David Cisneros <d.cisneros849@gmail.com>
2017-01-27 18:38:40 -0800

    incorporated new weather api

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -6,21 +6,21 @@ $(function() {
     var latitude = $(".latitude").text();
     var gameDate = $(".gameDate").text();
 
-      $.getJSON( "http://api.openweathermap.org/data/2.5/forecast/daily?lat="+latitude+"&lon="+longitude+"&cnt=10&units=imperial&APPID=e1b0f333867c7cac1ca29c6d5fb73b29", function(data) {
-        weatherForecast = data.list
+      $.getJSON( "https://api.apixu.com/v1/forecast.json?key=c24039352d9f4dfe9a3124854172701&q="+latitude+","+longitude+"&days=10", function(data) {
+        console.log(data);
 
-        for(var i=0;i<weatherForecast.length;i++) {
-          var unix_timestamp = weatherForecast[i].dt
-          var date = new Date(unix_timestamp * 1000);
-          var formattedDate = ('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear()
-
-            if (formattedDate == gameDate) {
-              var temp = weatherForecast[i].temp.max
-              var icon = weatherForecast[i].weather[0].icon
-
-              $("div.gameDetails").append("<img src='http://openweathermap.org/img/w/" + icon + ".png' alt='Icon depicting current weather.'>", temp);
-            }
-          }
+        // for(var i=0;i<weatherForecast.length;i++) {
+        //   var unix_timestamp = weatherForecast[i].dt
+        //   var date = new Date(unix_timestamp * 1000);
+        //   var formattedDate = ('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear()
+        //
+        //     if (formattedDate == gameDate) {
+        //       var temp = weatherForecast[i].temp.max
+        //       var icon = weatherForecast[i].weather[0].icon
+        //
+        //       $("div.gameDetails").append("<img src='http://openweathermap.org/img/w/" + icon + ".png' alt='Icon depicting current weather.'>", temp);
+        //     }
+        //   }
        });
       }
       getWeather();
