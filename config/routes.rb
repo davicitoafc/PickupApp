7a5d153aae51aca9f4db02ab629bd32e0daf0aec
David Cisneros <d.cisneros849@gmail.com>
2018-01-08 20:21:59 -0500

    added privacy policy because facebook is whack

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   resources :users, :only => [:show]
   resources :games
+  get "/pages/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
